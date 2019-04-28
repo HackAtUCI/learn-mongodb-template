c6bdfa01d09722d72b6bc3ef527a8423f990e2fc
Paul Yang <palyang777@gmail.com>
2019-04-28 02:27:13 -0700

    Added TO-DOs structure

--- db.py
+++ db.py
@@ -1,80 +1,88 @@
 import csv
-
 import pymongo
 from mlab_credentials import credentials
 
-'''
-Process:
-Create mlab 
-make a database
-create dbuser
-
-discuss use of system collections
-
-Powerpoint
-- Go over scalability
-- Go over advantages SQL vs No SQL (demo if you are ready)
-- Describe applications of No SQL (Pick a hot topic to grab people attention)
-- Give a brief history of mongodb (if applicable)
-
-Demo
-    Note:  Put fully working implementation above the skeleton code, create a class to manage database, Create some exercises
-
-    -  Demo how to query the data (quick demo on how to use this data would be nice)   (Use filter)
-    -  Demo how to update the data    (Use filter)
-    -  Demo how to delete the data    (Use filter)
-
-'''
-
 if __name__ == "__main__":
+
     uri = "mongodb://{dbuser}:{dbpassword}@ds039007.mlab.com:39007/hack-roles".format(dbuser = credentials["username"], dbpassword = credentials["password"])
     client = pymongo.MongoClient(uri)
     db = client["hack-roles"]
-    role_info = db["role-info"]
+    role_info = db["role-info"]     # Collection functions: https://api.mongodb.com/python/current/api/pymongo/collection.html
+
 
 
     # *CREATING DOCUMENTS* #
+    # TO-DO (1): Insert data into database using csv.DictWriter (comment out code after inserted to avoid multiple insertions)
+    #
     # csv_file = open("roles.csv") 
     # reader = csv.DictReader(csv_file)   # will use the fieldnames in the first row to create dict for each row 
     # for row in reader:                  # could use insert_many() if you had a multiple dicts in a list
     #     role_info.insert_one(row)
+    # csv_file.close()
+
+
 
     # *READING DOCUMENTS* #
-    # filter = {"committees": {"regex": "^c.+"} }
+    #
+    # TO-DO (2): Read all data from database 
+    # for i in role_info.find():
+    #     print(i)
+    #
+    # TO-DO (3): Print data that has no qualifications
+    # filter = {"qualifications": "[]" }
     # for i in role_info.find(filter):
     #     print(i)
-    # # find_one is pretty much the same thing, but find the first document with the specifications
-
-    # just shows prompts -> could process the string and get the links and scrape them
-    # for i in role_info.find({}, {"_id": 0, "prompt":1}):
+    #
+    # TO-DO (4): Read data that has something in its qualification header with a query selector
+    # filter = {"qualifications": {"$regex": "[A-Za-z]+"}}
+    # for i in role_info.find(filter):
     #     print(i)
+    #
+    # TO-DO (5): Read same data as TO-DO (4), but omit everything except the prompt
+    # filter = {"qualifications": {"$regex": "[A-Za-z]+"}}
+    # for i in role_info.find(filter, {"_id": 0, "prompt": 1}):
+    #     print(i)
+
 
 
     # *UPDATING DOCUMENTS* #
-    role_info.update_one({"committee":"marketing"}, {"$set": {"foo": "bar"}})
-    for i in role_info.find({}):
-        print(i)
+    # TO-DO (6): Insert {"foo":"bar"} into first document with committee marketing and print it out to see update
+    # role_info.update_one({"committee":"marketing"}, {"$set": {"foo": "bar"}})
+    # print(role_info.find_one({"committee":"marketing"}))
+    #
+    # TO-DO (7): Rename the "role" field to "title"
+    # role_info.update_many({}, {"$rename": {"role" : "title"}})
+    # for i in role_info.find():
+    #     print(i)
+    # 
+    # TO-DO (8): Take out the description field for all technology roles
+    # print(role_info.update_many({"committee":"technology"}, {"$unset":{"description":""}}).modified_count) # show how many documents are changed
+    # for i in role_info.find({"committee":"technology"}, {"description": 1}):
+    #     print(i)
+
+
 
     # *DELETING DOCUMENTS* #
-    role_info.delete_one()
+    # TO-DO (9): Delete the first document
+    # print(role_info.delete_one())
+    # TO-DO (10): Delete any documents with no deliverable prompt
+
+
 
 
     # *SORTING DOCUMENTS* #
+    #
+    # TO-DO ():
     # for i in role_info.find({}, {"_id": 0, "committee":1}).sort("committee", -1):
     #     print(i)
+    #
+    # TO-DO ():
     # for i in role_info.find({}, {"_id": 0, "committee":1}).sort("committee", 1):
     #     print(i)
 
     
     # *Exercises* #
 
-  
-
-    # https://api.mongodb.com/python/current/api/pymongo/collection.html
-    # https://docs.mongodb.com/manual/reference/operator/aggregation/filter/
-    # https://www.w3schools.com/python/python_mongodb_delete.asp
-
-    # role_info.delete_many({}) # deletes all documents 
-    # csv_file.close()
+    #role_info.delete_many() # deletes all documents 
     client.close()
 
