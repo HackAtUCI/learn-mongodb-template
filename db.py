90dd6924119a0ad0b400f6dc101e752c23e5a3be
raopg <prateek.rao.08@gmail.com>
2019-04-29 22:14:59 -0700

    Updated TODO syntax and comments

--- db.py
+++ db.py
@@ -11,75 +11,75 @@ if __name__ == "__main__":
 
 
 
-    # *CREATING DOCUMENTS* #
-    # TO-DO (1): Insert data into database using csv.DictWriter (comment out code after inserted to avoid multiple insertions)
-    #
+    # ## *CREATING DOCUMENTS* ##
+    # #TODO (1): Insert data into database using csv.DictWriter (comment out code after inserted to avoid multiple insertions)
+    
     # csv_file = open("roles.csv") 
     # reader = csv.DictReader(csv_file)   # will use the fieldnames in the first row to create dict for each row 
     # for row in reader:                  # could use insert_many() if you had a multiple dicts in a list
     #     role_info.insert_one(row)
     # csv_file.close()
 
-    # *READING DOCUMENTS* #
-    #
-    # TO-DO (2): Read all data from database 
+    # ## *READING DOCUMENTS* ##
+    
+    # #TODO (2): Read all data from database 
     # for i in role_info.find():
     #     print(i)
-    #
-    # TO-DO (3): Print data that has no qualifications
+    
+    # #TODO (3): Print data that has no qualifications
     # filter = {"qualifications": "[]" }
     # for i in role_info.find(filter):
     #     print(i)
-    #
-    # TO-DO (4): Read data that has something in its qualification header with a query selector
+    
+    # #TODO (4): Read data that has something in its qualification header with a query selector
     # filter = {"qualifications": {"$regex": "[A-Za-z]+"}}
     # for i in role_info.find(filter):
     #     print(i)
-    #
-    # TO-DO (5): Read same data as TO-DO (4), but omit everything except the prompt
+    
+    # #TODO (5): Read same data as TO-DO (4), but omit everything except the prompt
     # filter = {"qualifications": {"$regex": "[A-Za-z]+"}}
     # for i in role_info.find(filter, {"_id": 0, "prompt": 1}):
     #     print(i)
 
 
 
-    # *UPDATING DOCUMENTS* #
-    # TO-DO (6): Insert {"foo":"bar"} into first document with committee marketing and print it out to see update
+    # ## *UPDATING DOCUMENTS* ##
+    # #TODO (6): Insert {"foo":"bar"} into first document with committee marketing and print it out to see update
     # role_info.update_one({"committee":"marketing"}, {"$set": {"foo": "bar"}})
     # print(role_info.find_one({"committee":"marketing"}))
-    #
-    # TO-DO (7): Rename the "role" field to "title"
+    
+    # #TODO (7): Rename the "role" field to "title"
     # role_info.update_many({}, {"$rename": {"role" : "title"}})
     # for i in role_info.find():
     #     print(i)
-    # 
-    # TO-DO (8): Take out the description field for all technology roles
+    
+    # #TODO (8): Take out the description field for all technology roles
     # print(role_info.update_many({"committee":"technology"}, {"$unset":{"description":""}}).modified_count) # show how many documents are changed
     # for i in role_info.find({"committee":"technology"}, {"description": 1}):
     #     print(i)
 
 
 
-    # *DELETING DOCUMENTS* #
-    # TO-DO (9): Delete the first document
+    # ## *DELETING DOCUMENTS* ##
+    # #TODO (9): Delete the first document
     # print(role_info.delete_one())
-    # TO-DO (10): Delete any documents with no deliverable prompt
+    # #TODO (10): Delete any documents with no deliverable prompt
     # role_info.delete_many({"prompt":"[]"})
 
 
 
-    # *SORTING DOCUMENTS* #
-    #
-    # TO-DO (11): Print all data in database sorted by committee in reverse order
+    # ## *SORTING DOCUMENTS* ##
+    
+    # #TODO (11): Print all data in database sorted by committee in reverse order
     # for i in role_info.find({}, {"_id": 0, "committee":1}).sort("committee", -1):
     #     print(i)
-    #
-    # TO-DO (12): Print all data sorted by committee 
+    
+    # #TODO (12): Print all data sorted by committee 
     # for i in role_info.find({}, {"_id": 0, "committee":1}).sort("committee", 1):
     #     print(i)
     
-    # *Exercises* #
+    # ## *Exercises* ##
 
-    #role_info.delete_many() # deletes all documents 
+    role_info.delete_many() # deletes all documents 
     client.close()
 
